7f244c792f8ab7145809a7edf838c830084b806b
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-05-18 16:13:19 +0700

    Up ⭐

--- 11220910000042/konten/galeri.php
+++ 11220910000042/konten/galeri.php
@@ -1,5 +1,34 @@
+<?php
 
-            <p>Ini Halaman Galeri xxxxxxxxxxxxxxxxxxxxx</p>
-            <p>Ini Halaman Galeri xxxxxxxxxxxxxxxxxxxxx</p>
-            <p>Ini Halaman Galeri xxxxxxxxxxxxxxxxxxxxx</p>
-            <p><img src="images/gambar.png" alt="Ini gambar" width="145" height="117"></p>
+$username = '';
+if (isset($_GET['username'])) {
+    $username = $_GET['username'];
+}
+$result = mysqli_query($conn, "SELECT * FROM register WHERE `username` like $username");
+
+?>
+
+<style>
+    #data_center {
+        width: 810;
+        grid-template-columns: auto auto auto;
+    }
+    #data_user {
+        
+    }
+</style>
+
+Cari member
+<form action="" method="get">
+    <input type="hidden" name="module" value="gambar">
+    <input type="text" name="username">
+    <input type="submit" value="cari">
+</form>
+
+<div id="data_center">
+    <?php while($data = mysqli_fetch_assoc($result)): ?>
+        <div id="data_user">
+            <img src="" alt="">
+        </div>
+    <?php endwhile; ?>
+</div>
