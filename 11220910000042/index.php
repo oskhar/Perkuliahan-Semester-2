449ba08b434ae5779f42585b31cd36dfaf9ac2a5
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-05-25 15:23:18 +0700

    Up ⭐

--- 11220910000042/index.php
+++ 11220910000042/index.php
@@ -19,24 +19,37 @@
             <h1>PERTEMUAN I</h1>
         </div>
 
-        <div style="display: flex;">
-        <div id="sidebar">
-            <h3>navigasi</h3>
-            <ul id="navmenu">
-                <li><a href="?" <?php if (!isset($_GET['module'])) echo 'class="selected"'; ?>>Register</a></li>
-                <li><a href="?module=galeri" <?php if (isset($_GET['module'])) if ($_GET['module'] == "galeri") echo 'class="selected"'; ?>>Cari Data</a></li>
-                <li><a href="?module=jadwal" <?php if (isset($_GET['module'])) if ($_GET['module'] == "jadwal" || $_GET['module'] == "edit") echo 'class="selected"'; ?>>Pusat Data</a></li>
-            </ul>
-        </div>
+        <div id="badan">
+            <div>
+                <div id="sidebar">
+                    <h3>navigasi</h3>
+                    <ul id="navmenu">
+                        <li><a href="?" <?php if (!isset($_GET['module'])) echo 'class="selected"'; ?>>Register</a></li>
+                        <li><a href="?module=galeri" <?php if (isset($_GET['module'])) if ($_GET['module'] == "galeri") echo 'class="selected"'; ?>>Cari Data</a></li>
+                        <li><a href="?module=jadwal" <?php if (isset($_GET['module'])) if ($_GET['module'] == "jadwal" || $_GET['module'] == "edit") echo 'class="selected"'; ?>>Pusat Data</a></li>
+                    </ul>
+                </div>
 
-        <div id="page">
-            <?php
-            if (isset($_GET['module']))
-                include("konten/$_GET[module].php");
-            else
-                include("home.php");
-            ?>
-        </div>
+                <div id="login">
+                    <form action="" action="post">
+                        <input type="hidden" name="module" value="indexproc">
+                        <label>Username</label>
+                        <input type="text" name="user">
+                        <label>Password</label>
+                        <input type="password" name="pass">
+                        <input type="submit" value="sub" style="width: 80px; background: #99ccff; border: 1px solid gray; border-radius:3px;margin-top:10px">
+                    </form>
+                </div>
+            </div>
+
+            <div id="page">
+                <?php
+                if (isset($_GET['module']))
+                    include("konten/$_GET[module].php");
+                else
+                    include("home.php");
+                ?>
+            </div>
         </div>
 
         <div id="clear"></div>
